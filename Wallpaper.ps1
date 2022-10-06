9f76a602805a339c7b61de1733335eea8c2b85d6
Earnhbry000 <90056887+Earnhbry000@users.noreply.github.com>
2022-10-06 12:45:10 -0400

    Create Wallpaper.ps1

---
+++ Wallpaper.ps1
@@ -0,0 +1,8 @@
+Function Set-WallPaper($Value)
+ {
+    Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+    rundll32.exe user32.dll, UpdatePerUserSystemParameters
+ }
+ 
+ 
+ Set-WallPaper -value "path to wallpaper"
