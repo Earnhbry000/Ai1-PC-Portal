ef8be9f28e03d2dd6f70a4f88e3e664859f99f96
Earnhbry000 <90056887+Earnhbry000@users.noreply.github.com>
2022-10-07 09:40:24 -0400

    Update Wallpaper.ps1

--- Wallpaper.ps1
+++ Wallpaper.ps1
@@ -1,8 +1,13 @@
-Function Set-WallPaper($Value)
- {
-    Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
-    rundll32.exe user32.dll, UpdatePerUserSystemParameters
- }
+#Function Set-WallPaper($Value)
+ #{
+  #  Set-ItemProperty -path 'HKCU:\Control Panel\Desktop\' -name wallpaper -value $value
+    #rundll32.exe user32.dll, UpdatePerUserSystemParameters
+ #}
  
  
- Set-WallPaper -value "C:\Users\Public\Desktop\A.png"
+ #Set-WallPaper -value "C:\Users\Public\Desktop\A.png"
+
+
+reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d C:\Users\Public\Desktop\A.png /f
+ Start-Sleep -s 10
+ rundll32.exe user32.dll, UpdatePerUserSystemParameters, 0, $false
